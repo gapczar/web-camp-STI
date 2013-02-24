e993059e2cc835829a5d9178db0a6334d02b7d49
leonyl <skatzleonyl09@gmail.com>
2013-02-24 12:48:01 +0800

    oew

--- web/index.php
+++ web/index.php
@@ -2,7 +2,12 @@
 <?php
 include ("connection.php");
 if(isset($_SESSION['user_id'])){
-	
+	echo $_SESSION["user_id"];
+
+	$getuser = "select * from users where userid = '" . $_SESSION["user_id"] . "'";
+	$userqry = mysql_query($getuser) or die(mysql_error());
+	$userdata = mysql_fetch_assoc($userqry);
+	echo $userdata["username"];
 }
 ?>
 <html>
@@ -15,8 +20,8 @@ if(isset($_SESSION['user_id'])){
 <?php
 	include "registration_form.php";
 	include "login_form.php";
+
+	
 ?>
-
-
 </body>
 </html>
