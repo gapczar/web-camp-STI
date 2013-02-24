aaaa24ecd7d4366071c4e6cd0d776def679aaaf6
GAPLabs <gaplabs@Vincent.local>
2013-02-24 13:12:23 +0800

    asd

--- web/index.php
+++ web/index.php
@@ -1,27 +1,152 @@
 <!DOCTYPE html>
-<?php
-include ("connection.php");
-if(isset($_SESSION['user_id'])){
-	echo $_SESSION["user_id"];
-
-	$getuser = "select * from users where userid = '" . $_SESSION["user_id"] . "'";
-	$userqry = mysql_query($getuser) or die(mysql_error());
-	$userdata = mysql_fetch_assoc($userqry);
-	echo $userdata["username"];
-}
-?>
 <html>
 	<head>
+
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css"  href="css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css"  href="css/bootsrap.min.css">
+<link rel="stylesheet" type="text/css"  href="css/bootstrap-responsive.min.css">
+<link rel="stylesheet" type="text/css"  href="css/style.css">
+
 		<script type='text/javascript' src='/functions/jquery.js'></script>
 		<script type='text/javascript' src='/functions/index_func.js'></script>
+		
+		<script type='text/javascript' src='/js/bootstrap-modal.js'></script>
+
 	</head>
 <body>
 	
-<?php
-	include "registration_form.php";
-	include "login_form.php";
 
-	
+<?php
+//	include "registration_form.php";
 ?>
+		
+
+
+
+
+<header class="top-header">
+
+
+<h1 class="title-bar"> POLL WEBSITE FOR MOBILE APPS </h1>
+
+
+<a href="#myModal" role="button" class="btn-login" data-toggle="modal">Login</a>
+
+
+
+
+
+ 
+<!-- Modal -->
+<div id="myModal" class="modal hide fade"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  
+
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
+    <h3  id="myModalLabel">Login</h3>
+  </div>
+
+
+  <div class="modal-body">
+    <p>
+<?php
+	include "login_form.php";
+?>
+
+<hr> <h4> <center> OR </h4> </hr>
+
+<?php
+  include"registration_form.php";
+
+?>
+
+    </p>
+  </div>
+ 
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+<header class="left-header">
+
+<h1 class="title-bar2">Apps </h1>
+
+<form class="form-search">
+  <input type="text" placeholder="Search" class="">
+  </form>
+<div class="nav-menu">
+
+<ul class="nav nav-list">
+  <li class="nav-header"></li>
+  <li class="active"><a href="#">Entertainment</a></li>
+  
+ <li class="nav-header"></li>
+  <li class="active"><a href="#">Social</a></li>
+  
+ <li class="nav-header"></li>
+  <li class="active"><a href="#">Utilities</a></li>
+  
+
+  
+</ul>
+
+</div>	
+
+
+</div>
+	</div>
+		</div>
+
+
+<div class="apps-icon">
+
+
+<div class="apps-box">
+  asdasdasdsa
+</div>
+<div class="apps-box">
+  asdasdasdsa
+</div>
+<div class="apps-box">
+  asdasdasdsa
+</div>
+<div class="apps-box">
+  asdasdasdsa
+</div>
+<div class="apps-box">
+  asdasdasdsa
+</div>
+<div class="apps-box">
+  asdasdasdsa
+</div>
+
+
+
+
+</div>
+
+
+
+</header>
+
+
 </body>
+
+
+
 </html>
--- web/login_form.php
+++ web/login_form.php
@@ -1,6 +1,44 @@
+
+
+<!DOCTYPE html>
+
+
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css"  href="css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css"  href="css/bootsrap.min.css">
+<link rel="stylesheet" type="text/css"  href="css/bootstrap-responsive.min.css">
+<link rel="stylesheet" type="text/css"  href="css/style.css">
+
+
+
+
+
+
 <form id='login_form'>
-		Uname:<input type='text' name='login_username' /><br/>
-		Pword:<input type='password' name='login_password'/><br/>
-		<a href='javascript:void(0);' id='login_handler'><div id='login_handler_btn'>Login</div></a>
+
+
+<div class="input-prepend">
+  <span class="add-on">Username</span>
+  <input class="pass1" id="prependedInput" type='text' name='login_username' placeholder='Username'>
+</div>
+<div class="input-prepend">
+  <span class="add-on">Password</span>
+  <input class="pass1" id="prependedInput" type='password' name='login_password'placeholder='Password'>
+
+
+
+  
+ 
+
+  <a href='javascript:void(0);' id='login_handler'>	<button id="login_handler_btn" class="btn1">Login</button></a>
 		<img src='loader.gif' style='width:20px;' id='login_loader' />
-</form>
+    <button class="btn1" data-dismiss="modal" aria-hidden="true">Cancel</button>
+    
+  
+</div>
+
+
+
+
+</form>	
