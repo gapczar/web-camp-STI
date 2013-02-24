5e46b2fd0d148e278324bd1408214ff0939a38c2
leonyl <skatzleonyl09@gmail.com>
2013-02-24 11:14:23 +0800

    asdf

--- web/index.php
+++ web/index.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
+<?php
+include ("connection.php");
+if($handle = opendir("../apps-list/images"))
+{
+	while(false !== ($entry = readdir($handle)))
+	{
+		$filedata = explode(".", $entry);
+		$filename = $filedata[0];
+		$filetype = $filedata[1];
+		$saveimgqry = "insert into mobile_app_img (mai_filename, mai_filetype) values ('" . $filename . "', '" . $filetype . "')";
+		$saveimg = mysql_query($saveimgqry) or die(mysql_error());
+	}
+}
+?>
 <html>
 	<head>
 		<script type='text/javascript' src='/functions/jquery.js'></script>
@@ -9,6 +23,7 @@
 <?php
 
 	include "registration_form.php";
+	include "login_form.php";
 ?>
 
 
